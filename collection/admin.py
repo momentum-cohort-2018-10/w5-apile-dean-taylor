913a1af755236f0caf521d587e5993779a53a468
BarstoolsDreamers <david@davidlndean.com>
2018-11-26 20:33:05 -0500

    Added Post Model, created Admin and Superuser

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from collection.models import Post
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('title', 'link', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from collection.models import Post
 
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'index.html', {
+        'post': posts
+    })
