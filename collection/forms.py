aaeca51f6f14fba68a9662089fb277d41db6e576
BarstoolsDreamers <david@davidlndean.com>
2018-11-28 09:15:50 -0500

    Added objects to home page and log-in required for editing / submitting posts.

---
+++ collection/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from collection.models import Post
+
+
+class PostForm(ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'link', 'description', 'author',)
