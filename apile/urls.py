55604e96c4374c45ddd8315802c55f60fa54b6eb
BarstoolsDreamers <david@davidlndean.com>
2018-12-03 05:57:39 -0500

    BIG changes. Pagination, sorting, edit post, create post all working...so close on votes. Trashed work on comments.

--- apile/urls.py
+++ apile/urls.py
@@ -36,9 +36,10 @@ urlpatterns = [
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('register/', TemplateView.as_view(template_name='register.html'),
          name='register'),
-    path('posts/<slug>/', views.post_detail, name='post_detail'),
-    path('accounts/create_post/', views.create_post,
-         name='create_post'),
+    # path('posts/<slug>/', views.post_detail, name='post_detail'),
+    path('posts/create/', views.create_post, name='create_post'),
+    path('posts/edit/<int:postid>/', views.edit_post, name='edit_post'),
+    path('posts/<int:postid>/', views.post_detail, name='post_detail'),
     path(
         'accounts/password/reset/',
         PasswordResetView.as_view(
@@ -64,9 +65,6 @@ urlpatterns = [
     path(
         'accounts/register/', MyRegistrationView.as_view(),
         name='registration_register'),
-    path(
-        'accounts/create_post/', views.create_post,
-        name='registration_create_post'),
     path('accounts/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
 
--- collection/admin.py
+++ collection/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from collection.models import Post
+from collection.models import Post, Vote
 # from django.contrib.auth.models import User
 
 
 class PostAdmin(admin.ModelAdmin):
     model = Post
     list_display = ('title', 'author', 'text')
-    prepopulated_fields = {'slug': ('title',)}
+    # prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Vote)
